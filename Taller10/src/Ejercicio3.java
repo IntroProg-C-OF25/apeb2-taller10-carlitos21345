717b2b6b58201a92e7af42660a59819ed25448b6
carlitos21345 <ismaelcarrillo380@gmail.com>
2025-01-05 22:28:09 -0500

    Nueva actualizacion

--- Taller10/src/Ejercicio3.java
+++ Taller10/src/Ejercicio3.java
@@ -1,4 +1,21 @@
+/***
+ * Ejercicio 3
+ *El primer ciclo paralelo C, cuenta con 28 estudiantes, de los cuáles al finalizar el periodo, 
+ * la Dirección de la carrera de Computación a solicitado las siguientes estadísticas de la materia INTRODUCCIÓN A LA PROGRAMACIÓN
+ * en función a los promedios por estudiante, dichos promedios se 
+ * deben calcular (ponderar, ya que el docente ingresa todo sobre 10pts.) de 3 calificaciones 
+ * (ACD que representa el 35% de la nota, APE del 35% y la nota del AA con un peso del 30%).
+ * En resumen, los requerimientos son los siguientes:
 
+ *Registre los nombres de cada estudiante de dicho paralelo.
+ *Genere aleatoriamente las notas ACD, APE, AA, para cada uno de los 28 estudiantes de 0-10 pts.
+ *Calcule el promedio de cada uno de los estudiantes del paralelo dada la siguiente ponderación: ACD->35%, APE->35%, y el AA->30%.
+ *Obtenga el promedio del curso, del paralelo C.
+ *Liste los nombres de los estudiantes y su nota, que hayan obtenido un promedio por encima del promedio del curso.
+ *Liste los nombres de los estudiantes y su nota, que hayan obtenido un promedio por debajo del promedio del curso.
+ *Muestre el estudiante con su calificación, si es el del mayor promedio (el más alto de la clase).
+ *Muestre el estudiante con su calificación, si es el del menor promedio (el más bajo de la clase).
+ */
 import java.util.Scanner;
 import java.util.Random;
 
